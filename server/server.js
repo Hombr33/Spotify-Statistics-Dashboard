2a16a255b670f5ea6fb95eefb2d082a097353b3b
hombr33 <damianradinoiu@outlook.com>
2026-01-26 17:46:54 +0000

    Testing FetchAccounts

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import pkg from "./connect.cjs";
+import cors from "cors";
+
 import fetchTopArtistsRoute from "./routes/fetchTopArtists.js"
 import fetchAccountsRoute from "./routes/fetchAccount.js"
 import loginRoute from "./auth/login.js"
@@ -17,6 +19,12 @@ connectMongo();
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use(cors({
+  origin: "https://reddit-statistics-app.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 app.use("/signup", signupRoute);
 app.use("/login", loginRoute);
