b1d208c24b7cfe3feb963123d0ff0cf7c335bde2
hombr33 <damianradinoiu@outlook.com>
2026-02-18 18:37:41 +0000

    Testing API return data

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,16 +1,18 @@
 
+import { CopyIcon } from '@radix-ui/react-icons';
 import { BsPerson } from 'react-icons/bs';
 
 interface ProfileProps {
-
     ProfileData: {
 
         display_name: string;
-
+        image: string;
+        followers: number;
+        external_urls: string;
     };
 }
 
-function Profile({ProfileData}: ProfileProps) {
+function Profile({ ProfileData }: ProfileProps) {
 
     return (
         <section className="Profile">
@@ -21,16 +23,13 @@ function Profile({ProfileData}: ProfileProps) {
                         Welcome {ProfileData?.display_name || 'user'}
                     </div>
 
-                    <div className="user-desc">
-                        <span> User description </span>
-                    </div>
-
                     <div className="user-small-stats">
+                     <a> {ProfileData?.external_urls || ''} </a> <CopyIcon/>
                     </div>
                 </div>
 
                 <div className="profile-img">
-                    <BsPerson style={{ fontSize: '50px' }} />
+                    {ProfileData?.image || <BsPerson style={{ fontSize: '50px' }} />}
                 </div>
             </div>
 
@@ -40,7 +39,7 @@ function Profile({ProfileData}: ProfileProps) {
 
                 <span id="stat"><div id="stat-number"> 10 </div> Playlists </span>
 
-                <span id="stat"><div id="stat-number">  </div> Followers </span>
+                <span id="stat"><div id="stat-number"> {ProfileData?.followers || '0'} </div> Followers </span>
 
             </div>
         </section>
