9a8c57cf4847c315bea87acda6b2afe7c488c2d4
hombr33 <damianradinoiu@outlook.com>
2026-02-25 17:47:19 +0000

    - Testing API routes

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,16 +1,13 @@
 
 import { CopyIcon } from "@radix-ui/react-icons";
 import { BsPerson } from "react-icons/bs";
-
-
-//Objects are not valid as a React child (found: [missing argument]). 
-// If you meant to render a collection of children, use an array instead.
+import { IoAlertOutline, IoSparkles } from "react-icons/io5";
 
 interface ProfileProps {
     ProfileData: {
         profileImage: Image[];
         display_name: string;
-        followers: {href: string; total: number};
+        followers: { href: string; total: number };
         external_urls: { spotify: string };
         product: string;
     };
@@ -35,21 +32,43 @@ function Profile({ ProfileData }: ProfileProps) {
                         Welcome,  {ProfileData?.display_name || 'user'} 👋
                     </div>
 
-                    <div className="user-link">
-                        <CopyIcon />
+                    <div className="user-info-container">
 
-                        {ProfileData?.external_urls?.spotify && (
-                            <a href={ProfileData.external_urls.spotify} target="_blank" rel="noreferrer">
-                                {ProfileData.external_urls.spotify}
-                            </a>
-                        )}
+                        <div className="user-link">
+                            {ProfileData?.external_urls?.spotify ? (
+                                <a href={ProfileData.external_urls.spotify} target="_blank" rel="noreferrer">
+                                    <CopyIcon />
+                                    {ProfileData.external_urls.spotify}
+                                </a>
+                            ) : (
+
+                                <a>
+                                    <IoAlertOutline />
+                                    Profile link not found
+                                </a>
+                            )}
+                        </div>
+
+                        <div className="user-accountBadge">
+                            {ProfileData?.product ? (
+                                <a>
+                                    <IoSparkles />
+                                    {ProfileData?.product}
+                                </a>
+                            ) : (
+                                <a>
+                                    <IoAlertOutline />
+                                    Account type not found
+                                </a>
+                            )}
+                        </div>
 
                     </div>
                 </div>
 
                 <div className="profile-img">
                     {profileImageUrl ?
-                        <img src={profileImageUrl} /> :
+                        <img src={profileImageUrl} alt="profile" width="200" height="200" /> :
                         <BsPerson />
                     }
                 </div>
@@ -64,9 +83,6 @@ function Profile({ ProfileData }: ProfileProps) {
 
             </div>
 
-            <div className="accountBadge">
-                {ProfileData?.product}
-            </div>
         </section>
     )
 }
